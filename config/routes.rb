02a52558c6d67762454e18227bdc1f6a6dbc71a8
Luciano Petillo <luciano.prnl@gmail.com>
2022-09-21 14:30:42 +0200

    company controller & model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :beers
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
