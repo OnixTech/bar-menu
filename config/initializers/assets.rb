cd00a80462aeb61cb8fc0f34840ddb43d508989b
Luciano Petillo <luciano.prnl@gmail.com>
2023-01-02 15:22:17 +0100

    asset pipeline confing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join("app", "javascript")
+
+Rails.application.config.assets.precompile += %w[*.scss]
