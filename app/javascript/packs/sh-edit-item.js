d3bbcf021569a18d42e437d55de7d262fc33be22
Luciano Petillo <luciano.prnl@gmail.com>
2023-01-07 01:49:06 +0100

    toggle items

--- app/javascript/packs/sh-edit-item.js
+++ app/javascript/packs/sh-edit-item.js
@@ -1,7 +1,14 @@
 
 function editItem(element){
-  var a = element.dataset.id;
-  let b = document.getElementsByClassName(`sh-items-li-${a}`)
-  console.log(b);
+  var id = element.dataset.id;
+  let p = document.getElementsByClassName(`sh-item-${id}`);
+  let input = document.getElementsByClassName(`sh-item-input-${id}`);
+  document.getElementsByClassName(`sh-btn-edit-${id}`).innerText = 'Lock';
+  for (let i = 0; i < p.length; i++) {  
+    p[i].style.display = "none";
+    input[i].style.display = "";
+  }
+  //btn.innerHTML = "SAVE";
+ // console.log(btn);
 }
 
