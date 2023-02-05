70c3fdf31e79d54114629cb8adaf7fe9fbe01812
Luciano Petillo <luciano.prnl@gmail.com>
2023-02-05 15:46:45 +0100

    search bar includ sections titles

--- app/javascript/packs/sh-searchbar.js
+++ app/javascript/packs/sh-searchbar.js
@@ -6,14 +6,37 @@ function searchbar(){
   userInputCapitalized = userInput.value.toUpperCase();      
   
   listedProductsId = document.getElementById("wrapper");
-  listedcards = listedProductsId.getElementsByClassName("list");       
+  listedcards = listedProductsId.getElementsByClassName("list");
+  
   var c = [].fill(false);
   for (let i = 0; i < listedcards.length; i++) {
-    taglist = listedcards[i].getElementsByClassName("sh-p-search");   
+    taglist = listedcards[i].getElementsByClassName("sh-p-search-title");
     
+    r = taglist[0].innerText.toUpperCase().indexOf(userInputCapitalized);
+    if(r > -1){
+      c[i] = true;
+      listedcards[i].style.display = "";
+    }else{
+      c[i] = false;
+      listedcards[i].style.display = "none";
+    }
+  }
+  if(c.includes(true)){
+    c.fill(false);
+  }else{
+    searchItems(listedcards, userInputCapitalized);
+  }
+}
+
+function searchItems(listedcards, userInputCapitalized){
+
+  var c = [].fill(false);
+  for (let i = 0; i < listedcards.length; i++) {
+    taglist = listedcards[i].getElementsByClassName("sh-p-search");
+    tagtitle = listedcards[i].getElementsByClassName("sh-p-search-title");
+
     for(let a = 0; a < taglist.length; a++){
       r = taglist[a].innerText.toUpperCase().indexOf(userInputCapitalized);
-
       if(r > -1){
         c[a] = true;
         taglist[a].style.display = "";
@@ -29,4 +52,5 @@ function searchbar(){
     }
     c.fill(false);
   }
+
 }
