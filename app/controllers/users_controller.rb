bfafd02866e107ffc4fd2cbd74eae61ba451c2e0
Luciano Petillo <luciano.prnl@gmail.com>
2023-02-20 18:24:52 +0100

    users list style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,21 @@ class UsersController < ApplicationController
     user_params
     user = User.find(@user[:user_id])
     authorize user
-    user.update(:active => @user[:active])
+    user.update({:active => @user[:active], :role_id => @user[:role]})
     redirect_to companies_path(current_user)
   end
 
   private
 
   def user_params
-    @user = params.require(:user).permit(:active, :user_id)
+    @user = params.require(:user).permit(:active, :role, :user_id)
+    if @user[:role] == "master"
+      @user[:role] = 1
+    elsif @user[:role] == "manager"
+      @user[:role] = 2
+    elsif @user[:role] == "visitor"
+      @user[:role] = 2
+    end
   end
 
 end
