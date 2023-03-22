7e119b31f56d043d089d862eb44a24e17de58820
Luciano Petillo <luciano.prnl@gmail.com>
2023-03-22 17:13:05 +0100

    show cancel btns added

--- app/javascript/packs/sh-edit-item.js
+++ app/javascript/packs/sh-edit-item.js
@@ -1,20 +1,31 @@
 
 function editItem(element){
   var id;
-  let p, input, btnEdit, btnSave;
+  let p, input, btnEdit, btnSave, ipts, itms,btns;
   
   id = element.dataset.id;
   p = document.getElementsByClassName(`sh-item-toggle-${id}`);
   input = document.getElementsByClassName(`sh-form-toggle-${id}`);
   btnEdit = document.getElementsByClassName(`sh-btn-edit-${id}`);
   btnDelete = document.getElementsByClassName(`sh-btn-delete-${id}`);
-  for (let i = 0; i < p.length; i++) {  
-    p[i].style.display = "none";
-    input[i].style.display = "";
+
+  if (input[0].style.display === "none"){
+    itms = "none";
+    ipts = "";
+    btns = "none";
+  }else{
+    itms = "";
+    ipts = "none";
+    btns = "";
   }
 
-  btnEdit[0].style.display = "none";
-  btnDelete[0].style.display = "none";
+  for (let i = 0; i < p.length; i++) {  
+    p[i].style.display = itms;
+    input[i].style.display = ipts;
+  }
+
+  btnEdit[0].style.display = btns;
+  btnDelete[0].style.display = btns;
 }
 
 
--- app/javascript/packs/sh-edit-menu.js
+++ app/javascript/packs/sh-edit-menu.js
@@ -1,13 +1,23 @@
 function editMenu(element){
     var id;
-    let p, input, btnEditDelete;
+    let p, input, btnEditDelete, itms, ipts, btns;
 
     id = element.dataset.id;
     p = document.getElementsByClassName(`sh-card-title-toggle-${id}`);
     input = document.getElementsByClassName(`sh-input-menu-title-toggle-${id}`);
     btnEditDelete = document.getElementsByClassName(`sh-card-edit-delete-toggle-${id}`);
     
-    p[0].style.display = "none";
-    input[0].style.display = "";
-    btnEditDelete[0].style.display = "none";
+    if (input[0].style.display === "none"){
+        itms = "none";
+        ipts = "";
+        btns = "none";
+      }else{
+        itms = "";
+        ipts = "none";
+        btns = "";
+      }
+
+    p[0].style.display = itms;
+    input[0].style.display = ipts;
+    btnEditDelete[0].style.display = btns;
 }
