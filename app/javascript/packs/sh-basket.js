08746b862744ace9cf50317a0250fce6adf6e52f
Luciano Petillo <luciano.prnl@gmail.com>
2023-07-04 14:47:09 +0200

    a tag unset, and order html stylesheet addded

--- app/javascript/packs/sh-basket.js
+++ app/javascript/packs/sh-basket.js
@@ -1,10 +1,11 @@
 
 function basketItems(event, element){
     event.preventDefault();
-
+    const itemId = element.getAttribute('data-id');
     const itemName = element.getAttribute('data-name');
     const itemPrice = parseFloat(element.getAttribute('data-price'), 10.00);
     const item = {
+        id: itemId,
         name: itemName,
         price: itemPrice
     };
@@ -15,9 +16,15 @@ const basket = {
     items: [],
     total: 0
 };
-
 function acumulator(item){
     basket.items.push(item);
     basket.total += item.price;
-    console.log(basket);
+    view(basket);
+}
+var myObject = JSON.parse(document.getElementById('my-object').value);
+function view(basket){
+
+    myObject = basket;
+    console.log(myObject);
+
 }
