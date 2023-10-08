93a7ba6393253e174a69cff810a768152e7b05c5
Luciano Petillo <luciano.prnl@gmail.com>
2023-10-08 13:46:34 +0200

    fix: post request

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -8,8 +8,8 @@ class DeliveriesController < ApplicationController
     def grequest
         skip_authorization
         #------------ Domains Config -------------#
-        policy_origin = "https://fillo.herokuapp.com/bsktresqto"
-        endpoint = "https://fillo-admin-2d8ec2766e12.herokuapp.com/bsktreq"
+        policy_origin = "http://127.0.0.1:3001/bsktresqto" #http://127.0.0.1:3001/bsktresqto https://fillo.herokuapp.com/bsktresqto
+        endpoint = "http://127.0.0.1:3000/bsktreq" #http://127.0.0.1:3000/bsktreq https://fillo-admin-2d8ec2766e12.herokuapp.com/bsktreq
         #----------------------------------------#
 
         #------------ Policy Config -------------#
