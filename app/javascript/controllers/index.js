48116969ba06180cef26e9ea0c5ea17665517263
Luciano Petillo <luciano.prnl@gmail.com>
2024-05-06 20:20:14 +0200

    nothing to commit

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import WebsocketStationController from "./websocket_station_controller"
+application.register("websocket-station", WebsocketStationController)
--- app/javascript/controllers/websocket_station_controller.js
+++ app/javascript/controllers/websocket_station_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     alert("ok");
+    console.log("hola");
   }
 }
