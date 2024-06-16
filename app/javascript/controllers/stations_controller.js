f7d04122fea2ddb8d41f230506282bb657b75806
Luciano Petillo <luciano.prnl@gmail.com>
2024-06-16 20:49:39 +0200

    test

--- app/javascript/controllers/stations_controller.js
+++ app/javascript/controllers/stations_controller.js
@@ -3,7 +3,6 @@ import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
   connect() {
-    alert("hello");
     createConsumer().subscriptions.create({channel: "StationChannel", room: this.element.dataset.stationid }, {
       received(data){
         if (data.action === "created"){
