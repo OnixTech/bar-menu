2e90d111990fef043d3d5ff4e8dcf7a62ce0d775
Luciano Petillo <luciano.prnl@gmail.com>
2024-06-20 21:02:39 +0200

    test: console.log station controller

--- app/javascript/controllers/stations_controller.js
+++ app/javascript/controllers/stations_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
   connect() {
     createConsumer().subscriptions.create({channel: "StationChannel", room: this.element.dataset.stationid }, {
       received(data){
+        console.log("Received data:", data);
         if (data.action === "created"){
           location.reload()
         }
