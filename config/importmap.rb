1e85d1095dd9b121318835e14f3d1561d11fee16
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-10 22:21:02 +0200

    fix: importmap add turbo-rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", to: "application.js"
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
