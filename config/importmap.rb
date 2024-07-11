4fc6cac5645993487770c185963bf8e8fc202bc9
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-11 22:52:38 +0200

    cable test

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "@rails/actioncable", to: "actioncable.esm.js", preload: true
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
