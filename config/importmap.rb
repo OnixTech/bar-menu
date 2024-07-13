3c6e454a5a075866b78a26fde6099ed25896183d
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-13 21:01:17 +0200

    fix: docker erase

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "application", to: "application.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/actioncable", to: "actioncable.esm.js"
