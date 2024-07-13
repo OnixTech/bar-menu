e075ffdfbbc5d0f1f399072318d522a4bd967f63
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-13 21:07:05 +0200

    test

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", to: "application.js", preload: true
+pin "application", to: "application.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
