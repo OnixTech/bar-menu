69678f687916db410aa78852dd21fa5cd6ca7173
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-28 16:56:42 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path    = require("path")
+onst path    = require("path")
 const webpack = require("webpack")
 
 module.exports = {
@@ -11,22 +11,6 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
-      '@hotwired/stimulus': path.resolve(__dirname, 'node_modules/@hotwired/stimulus'),
-      '@hotwired/stimulus-webpack-helpers': path.resolve(__dirname, 'node_modules/@hotwired/stimulus-webpack-helpers')
-    },
-    fallback: {
-      "path": require.resolve("path-browserify"),
-      "os": require.resolve("os-browserify/browser"),
-      "fs": false,
-      "dgram": false,
-      "net": false,
-      "tls": false,
-      "child_process": false
-    }
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
