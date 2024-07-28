44c62be062022dc78ec96d7f9c51c63f162b4bb4
Luciano Petillo <luciano.prnl@gmail.com>
2024-07-28 13:38:50 +0200

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/stimulus"
-import "@hotwired/stimulus-webpack-helpers"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 // Initialize Stimulus
 const application = Application.start()
