8d80102145dd9111c179457c1c1599d3d8af4be1
Luciano Petillo <luciano.prnl@gmail.com>
2024-08-01 15:44:23 +0200

    test-06

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { Application } from "@hotwired/stimulus";
+import "@hotwired/stimulus"
 import "./controllers"
-
-const application = Application.start();
