d4b513697f84c86ae1ed4dce56e499f0eff10166
Luciano Petillo <luciano.prnl@gmail.com>
2024-08-09 19:42:30 +0200

    fix: revert changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :delete_all
   has_many :order_histories
-  belongs_to :station_id
   validates :total, format: { with: /\A\d+(\.\d+)?\z/ }
 end
